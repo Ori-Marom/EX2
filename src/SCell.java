c05c3632e257764e53e702444624c3e8c31828da
Ori <orimarom101@gmail.com>
2025-01-02 20:20:23 +0200

    change for SCell

--- src/SCell.java
+++ src/SCell.java
@@ -2,6 +2,53 @@
 public class SCell implements Cell {
     private String line;
     private int type;
+
+
+    public SCell(String s) {
+        // Add your code here
+        setData(s);
+    }
+
+    @Override
+    public int getOrder() {
+        // Add your code here
+
+        return 0;
+        // ///////////////////
+    }
+
+    //@Override
+    @Override
+    public String toString() {
+        return getData();
+    }
+
+    @Override
+public void setData(String s) {
+        // Add your code here
+        line = s;
+        /////////////////////
+    }
+    @Override
+    public String getData() {
+        return line;
+    }
+
+    @Override
+    public int getType() {
+        return type;
+    }
+
+    @Override
+    public void setType(int t) {
+        type = t;
+    }
+
+    @Override
+    public void setOrder(int t) {
+        // Add your code here
+
+    }
     public static boolean isNumber(String text) {
         try {
             Double.parseDouble(text);
@@ -214,49 +261,4 @@ public class SCell implements Cell {
     }
 
 
-    public SCell(String s) {
-        // Add your code here
-        setData(s);
-    }
-
-    @Override
-    public int getOrder() {
-        // Add your code here
-
-        return 0;
-        // ///////////////////
-    }
-
-    //@Override
-    @Override
-    public String toString() {
-        return getData();
-    }
-
-    @Override
-public void setData(String s) {
-        // Add your code here
-        line = s;
-        /////////////////////
-    }
-    @Override
-    public String getData() {
-        return line;
-    }
-
-    @Override
-    public int getType() {
-        return type;
-    }
-
-    @Override
-    public void setType(int t) {
-        type = t;
-    }
-
-    @Override
-    public void setOrder(int t) {
-        // Add your code here
-
-    }
 }
